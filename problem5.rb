eae47d505e6098bff39bd82d65edfb36230eaf01
worst <jeremy.blackburn@gmail.com>
2010-02-16 16:11:49 -0500

    some work on problem 5... still thinking things through

--- problem5.rb
+++ problem5.rb
@@ -1,10 +1,13 @@
 #!/usr/bin/env ruby
 
+##############################################################################
 # 2520 is the smallest number that can be divided by each of the numbers from
 # 1 to 10 without any remainder.
 # 
 # What is the smallest number that is evenly divisible by all of the numbers
 # from 1 to 20?
+##############################################################################
+
 
 # NOTES:
 # We don't need to test all these numbers, as any number divisible by a
@@ -12,6 +15,20 @@
 #
 # so.. maybe first we find the composites?
 #
+# second thought: least common multiple?
+# third thought: no, that doesn't really make sense...
+# def lcm(a, b)
+#   (a * b).abs / gcd(a, b)
+# end
+# 
+# def gcd(a, b)
+#   return a if b.eql?(0)
+#   
+#   # ruby floors by default on integer division i think?
+#   return gcd(b, a - b * (a/b))
+# end
+
+
 h = {
       # 20 => [1, 2, 4, 5, 10],
       # 19 => [1, 19],
